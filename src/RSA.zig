ff9754dba637fcdd0c6f54a38c3ea5b8d958a260
IlTrevi <trev.davide@gmail.com>
2024-11-17 18:23:42 +0100

    changes

--- src/RSA.zig
+++ src/RSA.zig
@@ -2,16 +2,45 @@ const std = @import("std");
 const math = std.math;
 
 
-pub fn cryptMessage(allocator:std.mem.Allocator, msg: []const u8, keys: [2]u128) ![]u128 {
-    var encrypt = try allocator.alloc(u128, msg.len);
-    
-    // converti ogni byte del messaggio in una grandezza numerica e applica RSA
-    for (msg, 0..) |byte, i| {
-        const value: u128 = @intCast(@as(u128, byte)); 
-        encrypt[i] = math.pow(u128, value, keys[0]) % keys[1]; // cifratura RSA: c = m^e % n
+fn mod_exp(base: u128, exp: u128, mod: u128) u128 {
+    var result: u128 = 1;
+    var b: u128 = base % mod;
+    var e: u128 = exp;
+
+    while (e > 0) {
+        if (e % 2 == 1) {
+            result = (result * b) % mod;
+        }
+        b = (b * b) % mod;
+        e = e / 2; // Integer division
     }
-    return encrypt;
+    return result;
 }
+
+    pub fn rsa_encrypt(message: []const u8, e: u128, n: u128, alloc: std.mem.Allocator) ![]u128 {
+
+        var slice = try alloc.alloc(u128, message.len);
+        var i:u32 = 0;
+        for (message) |c| {
+            slice[i] = mod_exp(@intCast(c), e, n);
+            i += 1;
+        }
+        return slice;
+    }
+
+    pub fn rsa_decrypt(message: []const u128, e: u128, n: u128, alloc: std.mem.Allocator) ![]u128 {
+
+    var slice = try alloc.alloc(u128, message.len);
+    var i:u32 = 0;
+    for (message) |c| {
+        slice[i] = @intCast(mod_exp(c, e, n));
+        i += 1;
+    }
+    return slice;
+}
+
+
+
 
 pub fn getKeys(allocator: std.mem.Allocator, n:u64, difference: u64) ![2][2]u128 {
     const rand = std.crypto.random;
@@ -35,17 +64,19 @@ pub fn getKeys(allocator: std.mem.Allocator, n:u64, difference: u64) ![2][2]u128
     
     std.log.debug("firstPrime: {}, secondPrime: {}\n", .{primes[firstPrime],primes[secondPrime]});
     
-    var e = rand.intRangeAtMost(u128, 3, maximum-1);
+    var e:u128 = 3;
     while (maximum%e == 0) {
-        e = rand.intRangeAtMost(u128, 3, maximum-1);
+        e += 2;
     }
     
-    var d: u128 = maximum+1;
-    while (true) {
-        if (d%e == 0) {break;}
-        else d += maximum;
-    }
-    d = d/e;
+    var prova = try extended_gcd(e, maximum);
+
+
+
+
+    if (prova<0) prova *= -1;
+
+    const d:u128 = maximum-@as(u128,@intCast(prova));
 
     const publicKEY = [2]u128{e,key};
     const privateKEY  = [2]u128{d, key};
@@ -53,6 +84,38 @@ pub fn getKeys(allocator: std.mem.Allocator, n:u64, difference: u64) ![2][2]u128
     return [2][2]u128{publicKEY, privateKEY};
     
 }
+
+
+
+
+fn extended_gcd(a: i256, b: i256) !i256 {
+    var x0: i256 = 1;
+    var x1: i256 = 0;
+    var y0: i256 = 0;
+    var y1: i256 = 1;
+    var a_temp = a;
+    var b_temp = b;
+
+    while (b_temp != 0) {
+        const quotient = @divTrunc(a_temp , b_temp);
+        const temp_b = b_temp;
+        b_temp = @rem(a_temp , b_temp);
+        a_temp = temp_b;
+
+        const temp_x = x0 - quotient * x1;
+        x0 = x1;
+        x1 = temp_x;
+
+        const temp_y = y0 - quotient * y1;
+        y0 = y1;
+        y1 = temp_y;
+    }
+
+    return x0;
+}
+
+
+
 
 pub fn getPrimes(allocator: std.mem.Allocator, n: u64) ![]u64 {
     var primes = try allocator.alloc(bool, @intCast(@as(usize, n+1)));
--- src/main.zig
+++ src/main.zig
@@ -10,14 +10,22 @@ pub const log_level: std.log.Level = .info;
 pub fn main() !void {
 
     cronometro.start();
-    
     const keys = try RSA.getKeys(allocator, 300, 30);
-    std.debug.print("Generating keys took: {} ms\n", .{cronometro.elapsedTime()});
+    std.log.debug("Generating keys took: {} ms\n", .{cronometro.elapsedTime()});
+    std.log.debug("keys: {any}, {any}\n", .{keys[0][0], keys[1][1]});
 
-    const messaggio = "ciao come va"[0..];
+    const messaggio = "come stai, tanto tempo fa"[0..];
 
-    const mes = try RSA.cryptMessage(allocator, messaggio, keys[0]);
+
+
+    const mes = try RSA.rsa_encrypt(messaggio, keys[0][0], keys[0][1], allocator);
     defer allocator.free(mes);
 
-    std.log.info("Messaggio criptato: {}\n", .{mes});
+    std.log.info("Messaggio criptato: {any}\n", .{mes});
+
+    const chiaro = try RSA.rsa_decrypt(mes, keys[1][0], keys[1][1], allocator);
+    defer allocator.free(chiaro);
+
+    std.log.info("Messaggio in chiaro: {any}\n", .{chiaro});
+
 }
